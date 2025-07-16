34d0c4e301a8b0fca330255f76636f34809e0c73
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-07-16 08:48:22 -0400

    Adding open api

--- nest-js-sample/src/movie/movie.controller.ts
+++ nest-js-sample/src/movie/movie.controller.ts
@@ -8,10 +8,12 @@ import {
 	Post,
 	Put,
 	 } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';	 
 import { InputMovie } from './movie.interface';
 import { MovieService } from './movie.service';
 import { Movie } from './movie.entity';	 
 
+@ApiTags('movie')
 @Controller('movie')
 export class MovieController {
 
@@ -19,30 +21,35 @@ export class MovieController {
 
 
 
+	@ApiOkResponse({description: 'All available movies'})
 	@Get()
 	getAllMovies(): Promise<Movie[]> {
 		return this.movieService.getAllMovies();
 	}
 
 
+	@ApiOkResponse({description: 'The movie based on the id provided'})
 	@Get(':id')
 	getOneMovie(@Param('id') id: string): Promise<Movie | null> {
 		return this.movieService.getOneMovie(parseInt(id,10));
 	};
 
 
+	@ApiOkResponse({description: 'Nothing'})
 	@Delete(':id')
 	@HttpCode(204)
 	removeMovie(@Param('id') id: string): void {
 		this.movieService.removeMovie(parseInt(id,10));
 	}
 
+	@ApiOkResponse({description: 'The newly created movie'})
 	@Post()
 	createNewMovie(@Body() movie: InputMovie): Promise<Movie> {
 		return this.movieService.createNewMovie(movie);
 	}
 
 
+	@ApiOkResponse({description: 'The updated movie'})
 	@Put(':id')
 	updateMovie(@Param('id') id: string, @Body() movie: Movie): Promise<Movie> {
 		console.log('Updating',movie);
--- nest-js-sample/src/movie/movie.entity.ts
+++ nest-js-sample/src/movie/movie.entity.ts
@@ -1,14 +1,28 @@
+import { ApiProperty} from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 
 @Entity('movie')
 export class Movie {
 
+	@ApiProperty({
+		description: 'Id of the movie',
+		example: 1
+	})
 	@PrimaryGeneratedColumn()
 	id :number;
 
+
+	@ApiProperty({
+		description: 'Title of the movie',
+		example: 'Robin Hood'
+	})
 	@Column()
 	title: string;
 
+	@ApiProperty({
+		description: 'year of the movie',
+		example: 2025
+	})
 	@Column()
 	year: number;
 
