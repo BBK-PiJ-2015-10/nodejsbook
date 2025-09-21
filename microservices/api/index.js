af69f857600192dde1a549dad7cd6c0bb8e9d969
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-09-21 11:58:18 -0400

    wip impl of new gateway endpoints

--- microservices/api/index.js
+++ microservices/api/index.js
@@ -2,6 +2,7 @@ import express, {response} from 'express';
 import {expressjwt} from "express-jwt";
 import {router as userRouter} from "./user/index.js";
 import {router as loginRouter} from "./auth.js";
+import {router as movieRouter} from "./movie/index.js";
 import {getChannel, registerHandler} from "./connect.js";
 
 const channel = await getChannel();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/user', expressjwt({secret: 'secret', algorithms: ['HS256']}), userRouter);
+app.use('/movie', movieRouter);
 //app.use('/user', userRouter);
 
 app.use((err, request, response, next) => {
--- microservices/api/movie/controller.js
+++ microservices/api/movie/controller.js
@@ -2,21 +2,19 @@ import {getAll, create} from "./model.js";
 
 export async function getAllAction(request, response) {
     try {
-        console.log("Received request to fetch all movies");
         const movies = await getAll();
         response.json(movies);
     } catch (e) {
+        console.log(`Error due to ${e}`);
         response.status(500).json(e);
     }
 }
 
 export async function createAction(request, response) {
     try {
-        console.log(`Received request to create a movie`);
-        const movie = await create(request.body);
-        response.json(movie);
+        const newMovie = await create(request.body);
+        response.json(newMovie);
     } catch (e) {
-        console.log(`Failures due to  ${e}`);
         response.status(500).json(e);
     }
 }
