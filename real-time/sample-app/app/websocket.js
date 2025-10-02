ad99669e0750e41fe51d8f68d87e47de95ea0c0b
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-10-02 09:46:28 -0400

    web chat draft1

--- real-time/sample-app/app/websocket.js
+++ real-time/sample-app/app/websocket.js
@@ -1,19 +1,38 @@
 import {WebSocketServer} from 'ws';
 
 export default function init(app) {
+
     const wss = new WebSocketServer({port: 8181});
+
+    console.log('Initialized websocket on port:8181')
 
     const connections = [];
 
     wss.on('connection', (ws) => {
-        // pushes each connection to the array of connections
+        console.log(`Someone is connecting`);
+
+        if (ws.readyState === 1){
+            console.log('Received connection is ready');
+        } else {
+            console.info('Received connection is not ready');
+        }
+
         connections.push(ws);
 
-        connection.on('message', (message) => {
-            connections.forEach((connection) => {
-                connection.send && connection.send(message);
+        ws.on('message', (msg) => {
+            console.log(`Websocket server got a message: ${msg}`);
+            connections.forEach((con) => {
+                if (con.readyState === 1) { // Check if connection is open
+                    console.log(`Connection is open ${con} sending message : ${msg}`);
+                    con.send(msg);
+                } else {
+                    console.log(`Connection is closed ${con} not sending message : ${msg}`);
+                    //con.send(msg);
+                }
             });
         });
+
     });
+
 }
 
