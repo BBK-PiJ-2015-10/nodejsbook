7cc6ee88778ec341c5b07b0a2a6430fc4722b6e0
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-10-10 06:43:28 -0400

    real web chat

--- real-time/sample-app/app/websocket.js
+++ real-time/sample-app/app/websocket.js
@@ -6,30 +6,63 @@ export default function init(app) {
 
     console.log('Initialized websocket on port:8181')
 
-    const connections = [];
+    //const connections = [];
+    const connections = {};
 
     wss.on('connection', (ws) => {
         console.log(`Someone is connecting`);
 
-        if (ws.readyState === 1){
+        if (ws.readyState === 1) {
             console.log('Received connection is ready');
         } else {
             console.info('Received connection is not ready');
         }
 
-        connections.push(ws);
+        //connections.push(ws);
 
         ws.on('message', (msg) => {
             console.log(`Websocket server got a message: ${msg}`);
-            connections.forEach((con) => {
-                if (con.readyState === 1) { // Check if connection is open
-                    console.log(`Connection is open ${con} sending message : ${msg}`);
-                    con.send(msg);
+
+            const data = JSON.parse(msg);
+            console.log(`Websocket decoded json: ${data}`);
+            let msgToSend;
+
+            switch (data.type) {
+                case 'join':
+                    console.info(`I am on join with data.name ${data.name}`)
+                    connections[data.name] = ws;
+                    msgToSend = JSON.stringify({
+                        type: 'join',
+                        names: Object.keys(connections)
+                    });
+                    console.info(`I am on join with msgToSend ${msgToSend}`)
+                    break;
+                case 'msg':
+                    console.info('I am on msg')
+                    msgToSend = JSON.stringify({type: 'msg', name: data.name, msg: data.msg});
+                    break;
+            }
+
+            Object.values(connections).forEach((conn) => {
+                if (conn.readyState === 1) { // Check if connection is open
+                    console.log(`Connection is open ${conn} sending message : ${msgToSend}`);
+                    conn.send && conn.send(msgToSend);
                 } else {
-                    console.log(`Connection is closed ${con} not sending message : ${msg}`);
+                    console.log(`Connection is closed ${conn} not sending message : ${msgToSend}`);
                     //con.send(msg);
                 }
-            });
+            })
+
+
+            // connections.forEach((con) => {
+            //     if (con.readyState === 1) { // Check if connection is open
+            //         console.log(`Connection is open ${con} sending message : ${msg}`);
+            //         con.send(msg);
+            //     } else {
+            //         console.log(`Connection is closed ${con} not sending message : ${msg}`);
+            //         //con.send(msg);
+            //     }
+            // });
         });
 
     });
